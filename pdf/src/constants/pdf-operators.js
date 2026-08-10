baffb8ad9114b5083bbe6ca2c5ab6c02338852b9
Selahattin Aybars Yıldırım <82141359+AybarsYldrm@users.noreply.github.com>
2026-08-10 14:16:44 +0300

    Delete pdf directory

--- pdf/src/constants/pdf-operators.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-module.exports = {
-    SAVE_STATE: 'q', RESTORE_STATE: 'Q', LINE_WIDTH: 'w',
-    FILL_COLOR_RGB: 'rg', STROKE_COLOR_RGB: 'RG', 
-    RECTANGLE: 're', FILL: 'f', STROKE: 'S',
-    BEGIN_TEXT: 'BT', END_TEXT: 'ET', SET_FONT: 'Tf', 
-    TEXT_POSITION: 'Td', SHOW_TEXT: 'Tj' 
-};
